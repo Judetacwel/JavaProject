b99a300c65f9bb4870fe62bcea75058ddd578fa6
Jude <judetacwel007@gmail.com>
2023-03-29 16:24:46 -0400

    My third commit

---
+++ src/SamplePAck/SamplwPack.java
@@ -0,0 +1,11 @@
+package SamplePAck;
+
+public class SamplwPack {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My sample Class");
+
+	}
+
+}
